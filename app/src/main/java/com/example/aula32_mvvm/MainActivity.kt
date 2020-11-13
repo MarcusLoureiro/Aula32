a85628d5d5fa5f865aea84e4b442b2f8b7cf2788
Marcus Loureiro <marcushuriel08@hotmail.com>
2020-11-13 12:02:55 -0300

    jetpack

--- app/src/main/java/com/example/aula32_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/aula32_mvvm/MainActivity.kt
@@ -2,31 +2,76 @@ package com.example.aula32_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.activity.viewModels
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Arrays.fill
+import java.util.Collections.fill
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
-    private lateinit var tool: Toolbar
+    private val viewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        setContent {
+            MaterialTheme {
+                myApp(viewModel.soma)
 
-        viewModel.soma.observe(this, Observer {
-            tvRes.text = it.toString()
-        })
-
-        fbIncrement.setOnClickListener {
-            viewModel.incremento(1)
-        }
-
-        toolbar.setNavigationOnClickListener {
-            viewModel.refresh()
-
+            }
         }
     }
+
+    @Composable
+    fun myApp(somaData: LiveData<Int>){
+        val soma by somaData.observeAsState()
+
+        Scaffold(
+            topBar = {
+
+            },
+            bodyContent = {
+                Column (
+                    modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(soma.toString(), fontSize = 30.sp)
+                }
+
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = {
+                   viewModel.incremento(1)
+                }){
+
+                    Icon(Icons.Filled.Add)
+                }
+            }
+        )
+    }
+
+
 }
